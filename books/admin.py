2bb616a06fb307093b32e5946ff3c0bdee9c7f91
zlaja-billund <mail@zlatan.dk>
2025-10-03 12:23:37 +0200

    Admin added

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    exclude = []
+
+admin.site.register(Book, BookAdmin)
