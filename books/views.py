adb7a04e5edb0454f260cd2f5a89ee16a1fb0821
zlaja-billund <mail@zlatan.dk>
2025-10-09 15:59:12 +0200

    Filter added

--- books/views.py
+++ books/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import filters
+from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
+
+from .serializers import BookSerializer
+from .models import Book
+
 
 # Create your views here.
+class BookListView(generics.ListAPIView):
+    serializer_class = BookSerializer
+    model = Book
+    queryset = Book.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['genre', 'title']
